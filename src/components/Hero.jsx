2f19d07adea89ee9b69d84ce896b5b0d22b20baa
AnshTripathi <154746992+AnshTripathi6969@users.noreply.github.com>
2025-05-17 22:28:10 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <main className='relative w-full h-screen overflow-hidden flex justify-center mb-[10%]'>
-        <video src="public\videos\hero.mp4"
+        <video src="hero.mp4"
             autoPlay
             loop
             muted
